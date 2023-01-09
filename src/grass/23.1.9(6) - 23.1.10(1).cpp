678a5bf907ca3c3600ac67ef3c3140dccb905477
gold-su <pds020130@naver.com>
2023-01-09 14:49:25 +0000

    grass

---
+++ src/grass/23.1.9(6) - 23.1.10(1).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	//while문, do while문
+	//위 두개는 조건(i < 3)을 검사하는 부분만 사용함
+	int = 0; //for문도 소괄호에서 정의하지 않고, 미리 정의해도됨
+	//for(i; i < 3; i++){}//하지만 소괄호 안에서 정의하는 것이 for의 장점
+	while (i<3){
+		//code
+		i++ //증가 연산자를 while문 몸체안에 설정을 해줘야함 안그러면 무한반복
+	}
+	
+	
+	return 0;
+}
