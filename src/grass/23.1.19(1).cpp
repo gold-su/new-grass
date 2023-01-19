ecfd68571c2b2a042ec0cf66f63ecf97bc40a106
gold-su <pds020130@naver.com>
2023-01-19 13:58:40 +0000

    grass

---
+++ src/grass/23.1.19(1).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){ 
+	
+	/*
+	1.함수 정의 제공
+	2.함수 원형 제공
+	3.함수 호출
+	*/ 
+	//함수는 두가지로 나눔 
+	//1.리턴값이 있는 타입, 
+	/*
+	typeName functionName(parameterList)()
+	{
+	  statement(s);
+		return value;
+	}*/
+	//2.리턴값이 없는 타입
+	/*
+	void functionName(parameterList)()
+	{
+		statement(s);
+		return;//생략될 수 있음
+	} */
+	
+	
+	
+	
+	return 0;
+}
