79d3dbfecf2a9645528b2c9018748b167971904c
gold-su <pds020130@naver.com>
2023-01-24 14:48:28 +0000

    grass

---
+++ src/grass/23.1.24(2)_copy0.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+const int SIZE = 8;
+
+int main(){ 
+	
+	int arr[SIZE] = { 1,2,4,8,16,32,64,128};
+	INT sum = simArr(arr, SIZE);
+	
+	
+	return 0;
+}  
