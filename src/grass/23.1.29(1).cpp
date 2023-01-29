f728c7025f4f1a171a056c3c1ba2ad5235c2821f
gold-su <pds020130@naver.com>
2023-01-29 14:23:12 +0000

    grass

---
+++ src/grass/23.1.29(1).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){ 
+	//함수는 원본이 아닌 복사본을 대상으로 작업한다.
+	//배열을 리턴할수 없지만, 함수는 구조체를 리턴할수있다.
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}  
+              
